c560e22516358ddffe8acedc53b6f82011847b1c
Мария Добрынина <masha-krim@yandex.ru>
2022-12-29 00:16:35 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='app-example',
+    version='0.0.1',
+    description='FastApi app',
+    install_requires=[
+        'fastapi==0.88.0',
+        #Виртуальный сервер
+        'uvicorn==0.20.0',
+        #ORM, для общения с бд
+        'SQLAlchemy==1.4.45',
+        'pytest==7.2.0',
+        #Для http запросов
+        'requests==2.28.1',
+    ]
+    ,
+    scripts=['app/main.py', 'scripts/create_db.py']
+)
